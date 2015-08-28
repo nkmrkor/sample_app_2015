cf26999746c07a186621ed92215e701fd6ee341d
shibatak <kaoru-nakamura2@bm-sms.co.jp>
2015-08-28 19:01:16 +0900

    8.2.4の途中まで

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,12 @@ class SessionsController < ApplicationController
   def new
   end
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:passwooord])
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
   def destroy
   end
